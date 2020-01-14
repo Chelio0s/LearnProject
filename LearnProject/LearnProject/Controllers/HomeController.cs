2bee182d2ae8f47d55563541bdf8775b348412e4
Agvares <agvares_666@bk.ru>
2020-01-14 13:44:42 +0300

    Create Cart Page

--- LearnProject/LearnProject/Controllers/HomeController.cs
+++ LearnProject/LearnProject/Controllers/HomeController.cs
@@ -42,6 +42,11 @@ namespace LearnProject.Controllers
             return View();
         }
 
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
